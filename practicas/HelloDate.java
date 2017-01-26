98e358722933bcea3e796cfe41781b2a861321c4
NavaEdgardo <edgardoarturo.nava@gmail.com>
2017-01-26 12:02:14 -0600

    Practica 1

---
+++ practicas/HelloDate.java
@@ -0,0 +1,16 @@
+package hellodate;
+
+    import java.util.Date;
+
+public class HelloDate {
+
+    public static void main(String[] args) {
+            
+		System.out.println ("Hoy es: ");
+		System.out.println(new Date());
+    		
+    
+    
+    }
+    
+}  
