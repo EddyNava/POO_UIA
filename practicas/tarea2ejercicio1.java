0858526da9f3d8a7eed1bacf86e9aa0f16fb4f3c
NavaEdgardo <edgardoarturo.nava@gmail.com>
2017-02-01 12:04:20 -0600

    tarea2

---
+++ practicas/tarea2ejercicio1.java
@@ -0,0 +1,27 @@
+public class tarea2ejercicio1 {
+
+
+        public static void main(String[] args) {
+
+		  	byte 	bait 		= 127;
+            short 	chort 		= 32767;
+            int 	entero 		= 2147483647;
+            long 	largo 		= 9223372036854775807l;
+            char 	caracter 	= '2';
+            String	cadena1 	= null;
+            String	cadena2 	= "hola	java";
+            
+
+
+ 		System.out.println("byte: " + bait);
+		System.out.println("short: " + chort);
+		System.out.println("int: " + entero);
+		System.out.println("long: " + largo);
+		System.out.println("char: " + caracter);
+		System.out.println("String: " + cadena1);
+		System.out.println("String: " + cadena2);
+		
+
+                   }   
+               } 
+
---
+++ practicas/tarea2ejercicio2.java
@@ -0,0 +1,31 @@
+public class tarea2ejercicio2 {
+
+public static byte    bait; 
+public static short   chort; 
+public static int     entero; 
+public static long    largo;   
+public static double  doble;   
+public static char    caracter; 
+public static boolean booleano;
+public static String  cadena;
+
+        public static void main(String[] args) {
+
+
+            
+System.out.println("byte: " + bait);
+System.out.println("short: " + chort);
+System.out.println("int: " + entero);
+System.out.println("long: " + largo);
+System.out.println("double: " + doble);
+System.out.println("char: " + caracter);
+System.out.println("Boolean: " + booleano);
+System.out.println("String: " + cadena);
+
+
+            
+
+
+
+                   }   
+               } 
